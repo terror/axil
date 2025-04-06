624d9ef61eec0806f1971b828c399ffe32e4f16e
Liam <liam@scalzulli.com>
2025-04-06 19:54:00 -0400

    Add go, java, javascript, json, rust and typescript parsers

--- src/language.rs
+++ src/language.rs
@@ -2,13 +2,29 @@ use super::*;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub(crate) enum Language {
+  Go,
+  Java,
+  JavaScript,
+  Json,
   Just,
+  Rust,
+  Tsx,
+  TypeScript,
 }
 
 impl From<Language> for TreeSitterLanguage {
   fn from(language: Language) -> Self {
     match language {
+      Language::Go => tree_sitter_go::LANGUAGE.into(),
+      Language::Java => tree_sitter_java::LANGUAGE.into(),
+      Language::JavaScript => tree_sitter_javascript::LANGUAGE.into(),
+      Language::Json => tree_sitter_json::LANGUAGE.into(),
       Language::Just => unsafe { tree_sitter_just() },
+      Language::Rust => tree_sitter_rust::LANGUAGE.into(),
+      Language::Tsx => tree_sitter_typescript::LANGUAGE_TSX.into(),
+      Language::TypeScript => {
+        tree_sitter_typescript::LANGUAGE_TYPESCRIPT.into()
+      }
     }
   }
 }
@@ -18,16 +34,24 @@ impl TryFrom<PathBuf> for Language {
 
   fn try_from(value: PathBuf) -> Result<Self> {
     if let Some(extension) = value.extension() {
-      match extension.to_str() {
-        Some("just") => return Ok(Self::Just),
-        _ => {}
+      if let Some(extension) = extension.to_str() {
+        match extension.to_lowercase().as_str() {
+          "go" => return Ok(Self::Go),
+          "java" => return Ok(Self::Java),
+          "js" => return Ok(Self::JavaScript),
+          "json" => return Ok(Self::Json),
+          "just" => return Ok(Self::Just),
+          "rs" => return Ok(Self::Rust),
+          "ts" => return Ok(Self::TypeScript),
+          "tsx" => return Ok(Self::Tsx),
+          _ => {}
+        }
       }
     }
 
     if let Some(filename) = value.to_str() {
-      match filename {
-        "justfile" => return Ok(Self::Just),
-        _ => {}
+      if filename == "justfile" {
+        return Ok(Self::Just);
       }
     }
 
